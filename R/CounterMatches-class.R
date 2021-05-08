2bd23955ef19e2bd0422d4aa8b121d5162189ddd
daxus4 <davide.raffaellii@gmail.com>
2021-05-08 12:55:47 +0200

    roxygen countermatches

--- R/CounterMatches-class.R
+++ R/CounterMatches-class.R
@@ -1,6 +1,24 @@
-#Virtual class of object that can count overall number of sequences that match
-#in some regions
-setClass("CounterMatches", slots = list(arranged = "logical", reduced = "logical"), contains = c("VIRTUAL"))
+#' Virtual class of object that can count overall number of sequences that match
+#' in some regions
+#'
+#' You can choose to use two class that inherit from this virtual class. The
+#' class CounterMatchesMat is a matrix that contain the overall number of 
+#' sequences that match in the regions, it's easy and fast. When you have to use
+#' less memory and you know that the matrix can be scattered, you can use
+#' CounterMatchesList, which have a more complex internal structure and can be a
+#' little slower, but it allows you to save space in memory.
+#' 
+#' @slot arranged boolean. It say whether the structure is sorted by match
+#' number
+#' @slot reduced boolean. It say whether the sequences and the regions with no
+#' matches are deleted from the structure
+#' 
+#' @author Davide Raffaelli\cr Politecnico di Milano\cr Maintainer: Davide
+#' Raffaelli\cr E-Mail: <davide2.raffaelli@@mail.polimi.it>
+#'
+setClass("CounterMatches",
+         slots = list(arranged = "logical", reduced = "logical"),
+         contains = c("VIRTUAL"))
 
 #Method that reorder the object by the overall number of sequences that match
 #in some regions
