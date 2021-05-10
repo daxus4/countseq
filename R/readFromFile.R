570b294c1332abe647f5278ab4bb59025a009897
daxus4 <davide.raffaellii@gmail.com>
2021-05-10 12:27:59 +0200

    tests and roxygen

--- R/readFromFile.R
+++ R/readFromFile.R
@@ -1,7 +1,7 @@
 #' Read from a file a matrix of integers, with rownames and colnames
 #'
-#' This function read from a file a matrix obtained from 
-#' \code{\link{countSeqsMatrix}} and written into it or any other matrix which 
+#' This function read from a file a matrix obtained from
+#' \code{\link{countSeqsMatrix}} and written into it or any other matrix which
 #' contains only integers and with rownames and colnames
 #'
 #' @usage readFromFile(filename)
@@ -12,18 +12,18 @@
 #' @examples
 #'
 #' #Get a matrix of integers with rownames and colnames from matrix.txt
-#' mat <- readFromFile("matrix.txt")
-#'
+#' readFromFile(system.file("extdata", "matrix.txt", package = "countseq"))
+#' @importFrom utils read.table
 #' @export
 readFromFile <- function(filename){
   if(!is.character(filename)){
     stop("filename must be a character")
   }
-  mat <- read.table(filename, header = TRUE, sep = " ", row.names = 1)
+  mat <- utils::read.table(filename, header = TRUE, sep = " ", row.names = 1)
   if(!is.numeric(mat) | any(mat%%1) != 0) {
     stop("This file doesn't contain a matrix of integers")
   }
-  if(length(rownames(mat)) != nrow(mat) | 
+  if(length(rownames(mat)) != nrow(mat) |
      length(colnames(mat)) != ncol(mat)){
     stop("This file doesn't contain a matrix with rownames and colnames")
   }
--- R/writeToFile.R
+++ R/writeToFile.R
@@ -1,11 +1,11 @@
 #' Write to a file a matrix obtained from \code{\link{countSeqsMatrix}}
 #'
-#' This function write to a file a matrix obtained from 
-#' \code{\link{countSeqsMatrix}} or any other matrix which contains only 
+#' This function write to a file a matrix obtained from
+#' \code{\link{countSeqsMatrix}} or any other matrix which contains only
 #' integers and with rownames and colnames
 #'
 #' @usage writeToFile(matrix, filename)
-#' @param matrix A matrix returned from \code{\link{countSeqsMatrix}} or any 
+#' @param matrix A matrix returned from \code{\link{countSeqsMatrix}} or any
 #' matrix which contains only integers and with rownames and colnames
 #' @param filename string. Name of the file where the matrix will be saved
 #' @author Davide Raffaelli\cr Politecnico di Milano\cr Maintainer: Davide
@@ -16,21 +16,23 @@
 #' mat <- matrix(data = c(1,2,3,4), ncol = 2)
 #' rownames(mat) <- c('1','2')
 #' colnames(mat) <- c('1','2')
-#' 
+#'
 #' #Write mat to a file
 #' writeToFile(mat, "matrix.txt")
 #'
+#' @importFrom utils write.table
 #' @export
 writeToFile <- function(matrix, filename){
   if(!is.numeric(matrix) | any(matrix%%1) != 0) {
     stop("This function require that matrix is a matrix of integers")
   }
-  if(length(rownames(matrix)) != nrow(matrix) | 
+  if(length(rownames(matrix)) != nrow(matrix) |
      length(colnames(matrix)) != ncol(matrix)){
     stop("Every row/column must have a name")
   }
   if(!is.character(filename)){
     stop("filename must be a character")
   }
-  write.table(matrix, file = filename, row.names = TRUE, col.names = TRUE)
+  utils::write.table(matrix, file = filename, row.names = TRUE,
+                     col.names = TRUE)
 }
